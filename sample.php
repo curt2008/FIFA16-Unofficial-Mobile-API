16988fd6fa19bb0c5a67625a1aba61d106325acd
Curtis Crewe <curtis@budgetwebsitesolutions.co.uk>
2013-03-03 22:59:43 +0000

    Added a sample file

---
+++ sample.php
@@ -0,0 +1,50 @@
+<?php
+  require $_SERVER['DOCUMENT_ROOT']."/secure/core.php";
+	
+	//Sample search for Dimitar Berbatov
+	$search = $s->playersearch(0,1,'gold','f442','attacker','9','13','144','','','','');
+ 	$results = json_decode($search, true);
+ 	
+ 	//Clean results for viewing pleasure
+ 	$EASW_KEY = explode("=",$info['EASW_KEY']);
+	$EASF_SESS = explode("=",$info['EASF_SESS']);
+	$PHISH_KEY = explode("-",$info['PHISHKEY']);
+	$XSID = explode(":",$info['XSID']);
+ 	
+ 	$orig_formation = $results['auctionInfo'][0]['itemData']['formation'];
+ 	$remove_f = explode("f",$orig_formation);
+ 	$new_formation = wordwrap($remove_f[1],1,'-',true);
+?>
+<html>
+	<head>
+		<title>Sample FUT13 Page</title>
+	</head>
+
+	<body>
+		<?php
+			
+			//display the connection info on the screen
+ 			echo "<b>Your EA Connection Details</b>:<br />";
+ 			echo "EASW Key : " . $EASW_KEY[1] . "<br />";
+ 			echo "EASF Session : " . $EASF_SESS[1] . "<br />";
+			echo "Phishing Key : " . $PHISH_KEY[1] . "<br />";
+ 			echo "Session ID : " . $XSID[1] . "<br /><br />";
+ 			
+ 			//display your marketplace data
+ 			echo "<b>Your Marketplace Data</b>:<br />";
+ 			echo "Total Coins : ". $results['credits'] ."<br />";
+ 			echo "Total Bid Tokens : ". $results['bidTokens']['count'] ."<br /><br />";
+ 			
+ 			//sample marketplace search
+ 			echo "<b>Sample Marketplace Search</b>:<br />";
+ 			echo "Trade ID : ".$results['auctionInfo'][0]['tradeId']."<br />";
+ 			echo "Buy now price : ".$results['auctionInfo'][0]['buyNowPrice']."<br />";
+ 			echo "Seller Name : ".$results['auctionInfo'][0]['sellerName']."<br />";
+ 			echo "Formation : ".$new_formation."<br />";
+		?>
+	</body>
+	
+	<footer>
+		<h3>All Credits go to <a href="https://github.com/curt2008/Fifa13-Ultimate-Team-PHP-App">curt2008</a></h3>
+	</footer>
+</html>
