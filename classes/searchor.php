c019fb20d51b39d406be2ec9c612d9fd590407c0
sebastianbarfurth <sebastian@barfurth.de>
2013-03-29 12:41:26 -0500

    Added developmentsearch to functioner.php
    This is to get the sample in the main file working. It's just copy and paste with development written in it...

--- classes/searchor.php
+++ classes/searchor.php
@@ -203,5 +203,60 @@ class Searchor {
 		
 		return $EASSEARCH;
 	}
+	
+	public function developmentsearch($start = 0,$count = 15, $level, $cat, $minBid, $maxBid, $minBIN, $maxBIN) {
+		$searchurl = "https://utas.fut.ea.com/ut/game/fifa13/auctionhouse?";
+		//String that holds our search variables
+		$searchstring = "";
+		
+		//Add to the search string based on the variables passed
+		if ($level != "" && $level != "any"){
+			$searchstring .= "&lev=$level";
+		}
+		
+		if ($cat != "" && $cat != "any"){
+			$searchstring .= "&cat=$cat";
+		}
+		
+		if ($minBid > 0){
+			$searchstring .= "&micr=$minBid";
+		}
+		
+		if ($maxBid > 0){
+			$searchstring .= "&macr=$maxBid";
+		}
+		
+		if ($minBIN > 0){
+			$searchstring .= "&minb=$minBIN";
+		}
+		
+		if ($maxBIN > 0){
+			$searchstring .= "&maxb=$maxBIN";
+		}
+		
+		//create the final search string
+		$search = $searchurl . "type=development&blank=10&start=$start&num=$count" . $searchstring;
+		//Set the cookie data
+		$cookie_string = $this->EASW_KEY."; ".$this->EASF_SESS ."; ".$this->PHISHKEY;                                                                       
+		//Setup cURL HTTP request
+		$ch = curl_init($search);                                                                      
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                                                                                     
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+		curl_setopt($ch, CURLOPT_COOKIE, $cookie_string); 
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+			'Content-Type: application/json',                                                                                
+			'x-http-method-override: GET',
+			$this->XSID)                                                                       
+		);
+		
+		//Contains the JSON file returned from EA
+		$EASSEARCH = curl_exec($ch);
+		curl_close($ch);
+		
+		unset ($start,$count,$level,$cat,$minBid,$maxBid,$minBIN,$maxBIN, $ch, $cookie_string, $search, $searchstring);
+		
+		return $EASSEARCH;
+	}
 }
 ?>
