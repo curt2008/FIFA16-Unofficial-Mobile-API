f8409b8814d355c6723f7a60e5739ea887aca099
Curtis Crewe <curtis@budgetwebsitesolutions.co.uk>
2013-09-29 20:47:08 +0100

    FIFA 14

--- secure/core.php
+++ secure/core.php
@@ -1,17 +1,11 @@
 <?php
 	require "./secure/config.php";
 	require "./classes/connector.php";
-	require "./classes/tradeor.php";
 	require "./classes/eahashor.php";
-	require "./classes/searchor.php";
-	require "./classes/functionor.php";
 	
 	//initialise the classes
 	$eahashor = new EAHashor();
-	$f = new Functionor();
 	$hash = $eahashor->eaEncode($secret);
 	$c = new Connector($user, $password, $hash);
 	$info = $c->connect();
-	$s = new Searchor($info['EASW_KEY'], $info['EASF_SESS'], $info['PHISHKEY'], $info['XSID']);
-	$t = new Tradeor($info['EASW_KEY'], $info['EASF_SESS'], $info['PHISHKEY'], $info['XSID']);
 ?>
