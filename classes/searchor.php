d9f71ff7998e756f5e5cc570172c4cbf330a2b69
Curtis Crewe <curtis@budgetwebsitesolutions.co.uk>
2014-09-19 16:28:49 +0100

    Updated searchor to FIFA 15

--- classes/searchor.php
+++ classes/searchor.php
@@ -2,60 +2,41 @@
 
 	use Guzzle\Http\Client;
 	use Guzzle\Plugin\Cookie\CookiePlugin;
-	use Guzzle\Plugin\Cookie\CookieJar\ArrayCookieJar;
+	use Guzzle\Plugin\Cookie\CookieJar\FileCookieJar;
  
 	class Searchor {
   
   		private $nuc;
 		private $sess;
 		private $phish;
+		private $_cookieFile;
 	
 		//initialise the class
-		public function __construct($nuc, $sess, $phish) {
-			$this->nuc 	= $nuc;
-			$this->sess 	= $sess;
-			$this->phish 	= $phish;
-						
+		public function __construct($data) {
+			$this->nuc 	= $data['nuc'];
+			$this->sess 	= $data['sess'];
+			$this->phish 	= $data['phish'];
+			$this->console  = $data['console'];
+			if($data['cookie']) {
+				$this->_cookieFile = $data['cookie'];
+			}		
 		}
 		
-		public function Playersearch($macr,$micr,$num,$cat,$playStyle,$pos,$team,$league,$nation,$start,$lev,$minb,$maxb) {
+		public function getCredits() {
 			$client = new Client(null);
+			if($this->_cookieFile) {
+				$cookiePlugin = new CookiePlugin(new FileCookieJar($this->_cookieFile));
+        			$client->addSubscriber($cookiePlugin);
+			}
         		$client->setUserAgent("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36");
-			$url = "https://utas.s2.fut.ea.com/ut/game/fifa14/transfermarket?";
-			$searchstring = "";
-			if ($level != "" && $level != "any"){
-                        	$searchstring .= "&lev=$level";
-               		}
-                	if ($pos != "" && $pos != "any"){
-                        	if ($pos == "defense" || $pos == "midfield" || $pos == "attacker"){
-                                	$searchstring .= "&zone=$pos";
-                        	}else{
-                                	$searchstring .= "&pos=$pos";
-                        	}
-                	}
-                	if ($nation > 0){
-                        	$searchstring .= "&nat=$nation";
-                	}
-                	if ($league > 0){
-                        	$searchstring .= "&leag=$league";
-                	}
-                	if ($team > 0){
-                        	$searchstring .= "&team=$team";
-                	}
-                	if ($micr > 0){
-                        	$searchstring .= "&micr=$micr";
-                	}
-                	if ($macr > 0){
-                        	$searchstring .= "&macr=$macr";
-                	}
-                	if ($minb > 0){
-                        	$searchstring .= "&minb=$minb";
-                	}
-                	if ($maxb > 0){
-                        	$searchstring .= "&maxb=$maxb";
-                	}
-                	$search = $url . "type=player&start=$start&num=$num" . $searchstring;
-			$request = $client->post($search);
+			switch($this->console) {
+				case "XBOX":
+					$request = $client->post("https://utas.fut.ea.com/ut/game/fifa15/user/credits");
+				break;
+				case "PS":
+					$request = $client->post("https://utas.s2.fut.ea.com/ut/game/fifa15/user/credits");
+				break;
+			}
  			$request->addHeader('Origin', 'http://www.easports.com');
 			$request->setHeader('Referer', 'http://www.easports.com/iframe/fut/bundles/futweb/web/flash/FifaUltimateTeam.swf');
 			$request->addHeader('X-HTTP-Method-Override', 'GET');
@@ -67,31 +48,21 @@
 			return $json;
 		}
 		
-		public function Consumables($macr,$micr,$num,$cat,$start,$lev,$minb,$maxb) {
+		public function tradeInfo($tradeId) {
 			$client = new Client(null);
+			if($this->_cookieFile) {
+				$cookiePlugin = new CookiePlugin(new FileCookieJar($this->_cookieFile));
+        			$client->addSubscriber($cookiePlugin);
+			}
         		$client->setUserAgent("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36");
-			$url = "https://utas.s2.fut.ea.com/ut/game/fifa14/transfermarket?";
-			$searchstring = "";
-			if ($lev != "" && $lev != "any"){
-				$searchstring .= "&lev=$lev";
+			switch($this->console) {
+				case "XBOX":
+					$request = $client->post("https://utas.fut.ea.com/ut/game/fifa15/trade/status?tradeIds=".$tradeId);
+				break;
+				case "PS":
+					$request = $client->post("https://utas.s2.fut.ea.com/ut/game/fifa15/trade/status?tradeIds=".$tradeId);
+				break;
 			}
-			if ($cat != "" && $cat != "any"){
-				$searchstring .= "&cat=$cat";
-			}
-			if ($micr > 0){
-				$searchstring .= "&micr=$micr";//min bid
-			}
-			if ($macr > 0){
-				$searchstring .= "&macr=$macr";//max bid
-			}
-			if ($minb > 0){
-				$searchstring .= "&minb=$minb";
-			}
-			if ($maxb > 0){
-				$searchstring .= "&maxb=$maxb";
-			}
-			$search = $url . "type=development&blank=10&start=$start&num=$num" . $searchstring;
-			$request = $client->post($search);
  			$request->addHeader('Origin', 'http://www.easports.com');
 			$request->setHeader('Referer', 'http://www.easports.com/iframe/fut/bundles/futweb/web/flash/FifaUltimateTeam.swf');
 			$request->addHeader('X-HTTP-Method-Override', 'GET');
@@ -102,31 +73,34 @@
 			$json = $response->json();
 			return $json;
 		}
-			
-		public function Chemistry($macr,$micr,$num,$cat,$start,$lev,$minb,$maxb,$style) {
+		
+		public function playerSearch($playerId = 0, $minbin = 0, $maxbin = 0, $start = 0, $num = 16) {
 			$client = new Client(null);
-        		$client->setUserAgent("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36");	
-			$url = "https://utas.s2.fut.ea.com/ut/game/fifa14/transfermarket?";
+			if($this->_cookieFile) {
+				$cookiePlugin = new CookiePlugin(new FileCookieJar($this->_cookieFile));
+        			$client->addSubscriber($cookiePlugin);
+			}
+        		$client->setUserAgent("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36");
+        		switch($this->console) {
+				case "XBOX":
+					$url = "https://utas.fut.ea.com/ut/game/fifa15/transfermarket?";
+				break;
+				case "PS":
+					$url = "https://utas.s2.fut.ea.com/ut/game/fifa15/transfermarket?";
+				break;
+			}
 			$searchstring = "";
-			if ($lev != "" && $lev != "any"){
-				$searchstring .= "&lev=$lev";
-			}
-			if ($cat != "" && $cat != "any"){
-				$searchstring .= "&cat=$cat";
-			}
-			if ($micr > 0){
-				$searchstring .= "&micr=$micr";//min bid
-			}
-			if ($macr > 0){
-				$searchstring .= "&macr=$macr";//max bid
-			}
-			if ($minb > 0){
-				$searchstring .= "&minb=$minb";
-			}
-			if ($maxb > 0){
-				$searchstring .= "&maxb=$maxb";
-			}
-			$search = $url . "type=training&playStyle=$style&start=$start&num=$num" . $searchstring;
+                	if ($minbin > 0){
+                        	$searchstring .= "&minb=".$minbin;
+                	}
+                	if ($maxbin > 0){
+                        	$searchstring .= "&maxb=".$maxbin;
+                	}
+                	if ($playerId > 0) {
+                		$searchstring .= "&maskedDefId=".$playerId;
+                	}
+                	
+                	$search = $url . "type=player&start=$start&num=$num".$searchstring;
 			$request = $client->post($search);
  			$request->addHeader('Origin', 'http://www.easports.com');
 			$request->setHeader('Referer', 'http://www.easports.com/iframe/fut/bundles/futweb/web/flash/FifaUltimateTeam.swf');
