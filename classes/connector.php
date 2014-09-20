0de87f53c20eff869056f94753ce5de5f266cbb4
Curtis Crewe <curtis@budgetwebsitesolutions.co.uk>
2014-09-21 00:38:41 +0100

    Update connector.php

--- classes/connector.php
+++ classes/connector.php
@@ -21,7 +21,7 @@
     			"phishing" => "https://www.easports.com/iframe/fut15/p/ut/game/fifa14/phishing/question?_="
     		);
 
-    		public function __construct($loginDetails, $cookieFile) {
+    		public function __construct($loginDetails, $cookieFile = tmpfile()) {
         		$this->_loginDetails = $loginDetails;
         		$this->_cookieFile = $cookieFile;
     		}
